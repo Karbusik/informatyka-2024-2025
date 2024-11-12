4c0b2fc2c1ab6d6f951913e07c68047989b91b32
karbusik <karbo08@prokonto.pl>
2024-11-12 11:04:36 +0100

    komentarz

---
+++ lekcja 2/kalkulator bmi.cpp
@@ -0,0 +1,20 @@
+ #include <iostream>
+ using namespace std;
+int main (){
+ float waga;
+ float wzrost;
+ cout << " podaj wage (w kg)" << endl;
+ cout << " podaj wzrost (w cm)" << endl;
+ cin  >> waga;
+ cin >> wzrost;
+ int BMI;
+ 
+
+
+
+
+
+
+
+
+}
